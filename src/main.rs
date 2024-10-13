5c74d378d9964fbaf864bb9aad5a17b94007b7cd
daizyoo <daiki20070809@icloud.com>
2024-10-13 23:09:01 +0900

    [add] Adding a structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+mod room;
+
+use std::{io::Result, sync::Mutex};
+
+use actix_web::{web::scope, App, HttpServer};
+
+use room::Room;
+use serde::{Deserialize, Serialize};
+
+const ADDRESS: &str = "127.0.0.1:8080";
+
+struct RoomList(Mutex<Vec<Room>>);
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    name: String,
+}
+
+#[derive(Serialize)]
+struct Response<T> {
+    data: Option<T>,
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .app_data(RoomList(Mutex::new(Vec::new())))
+            .service(scope("/room"))
+    })
+    .bind(ADDRESS)?
+    .run()
+    .await?;
+
+    Ok(())
 }
